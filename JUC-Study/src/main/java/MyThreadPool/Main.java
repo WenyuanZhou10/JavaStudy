377b9a083bad0d8278853b4a364b2f2593f2e7df
wenyuan zhou <90307671+CHOU-illus10n@users.noreply.github.com>
2025-02-17 15:50:59 +0800

    线程池 —— 线程池各参数实现

--- JUC-Study/src/main/java/MyThreadPool/Main.java
+++ JUC-Study/src/main/java/MyThreadPool/Main.java
@@ -1,6 +1,7 @@
 package MyThreadPool;
 
-import java.sql.SQLOutput;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zwy
@@ -10,15 +11,16 @@ import java.sql.SQLOutput;
  */
 public class Main {
     public static void main(String[] args) {
-        MyThreadPool myThreadPool = new MyThreadPool();
+        MyThreadPool myThreadPool = new MyThreadPool(2, 4, 1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(2), new DiscardRejectHandle());
         for (int i = 0; i < 5; i++) {
+            int finalI = i;
             myThreadPool.execute(() -> {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                System.out.println(Thread.currentThread().getName());
+                System.out.println(Thread.currentThread().getName() +" "+ finalI);
             });
         }
         System.out.println("主线程没有被阻塞");
--- JUC-Study/src/main/java/MyThreadPool/MyThreadPool.java
+++ JUC-Study/src/main/java/MyThreadPool/MyThreadPool.java
@@ -2,9 +2,8 @@ package MyThreadPool;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zwy
@@ -14,26 +13,74 @@ import java.util.concurrent.BlockingQueue;
  */
 public class MyThreadPool {
 
-    BlockingQueue<Runnable> blockingDeque = new ArrayBlockingQueue<>(1024);
+    private final int corePoolSize;
+    private final int maxSize;
+    private final int timeOut;
+    private final TimeUnit timeUnit;
+    public final BlockingQueue<Runnable> blockingQueue;
+    private final RejectHandle rejectHandle;
+    public MyThreadPool(int corePoolSize, int maxSize, int timeOut, TimeUnit timeUnit, BlockingQueue<Runnable> blockingQueue, RejectHandle rejectHandle){
+        this.corePoolSize = corePoolSize;
+        this.maxSize = maxSize;
+        this.timeOut = timeOut;
+        this.timeUnit = timeUnit;
+        this.blockingQueue = blockingQueue;
+        this.rejectHandle = rejectHandle;
+    }
+    List<Thread> coreList = new ArrayList<>();
 
-    Thread thread = new Thread(()->{
-        while(true){
-            try {
-                Runnable command = blockingDeque.take();//阻塞等待任务
-                command.run();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+    List<Thread> supportList = new ArrayList<>();
+
+    void execute(Runnable command){
+        // 小于核心线程数时，创建一个新的线程
+        if(coreList.size() < corePoolSize){
+            Thread thread = new CoreThread();
+            coreList.add(thread);
+            thread.start();
         }
-    },"唯一线程");
-
-    {
-        thread.start();
+        if(blockingQueue.offer(command)){
+            return;
+        }
+        // 阻塞队列满了，将创建一个新的辅助线程
+        if(coreList.size() + supportList.size() < maxSize){
+            Thread thread = new supportThread();
+            supportList.add(thread);
+            thread.start();
+        }
+        if(!blockingQueue.offer(command)){
+            rejectHandle.reject(command, this);
+        }
     }
 
-    // 1 线程什么时候创建
-    // 2 线程runnable是什么
-    void execute(Runnable command){
-        boolean offer = blockingDeque.offer(command);
+    class CoreThread extends Thread{
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Runnable command = blockingQueue.take();//阻塞等待任务
+                    command.run();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+    //辅助线程阻塞获取不到任务一段时间后自动结束
+    class supportThread extends Thread{
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Runnable command = blockingQueue.poll(timeOut, timeUnit);//阻塞等待任务
+                    if(command == null){
+                        break;
+                    }
+                    command.run();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + "线程结束");
+        }
     }
 }
